a5932fecd5ef694b019b3479be45f236e8f1ca9d
Zhilei Zheng <zhileizheng@Zhileis-MBP.hsd1.pa.comcast.net>
2020-05-21 22:02:00 -0400

    update for citus

--- citus/main.sh
+++ citus/main.sh
@@ -1,51 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 host port dbname"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 dbname mode(cold/hot)"
     exit 1
 fi
 
-HOST=$1
-PORT=$2
-DBNAME=$3
+DBNAME=$1
+MODE=$2
 
 # log
 LOGFILE=bench.log
-
 ROOT=$(dirname $(readlink -f $0))
 TPCH_HOME=${ROOT}/../tpch
 RESULTS=${ROOT}/results
 
-function benchmark_run() {
-  # store the settings
-  psql -h $HOST -p $PORT -d $DBNAME -c "select name,setting from pg_settings" > $RESULTS/settings.log 2> $RESULTS/settings.err
-
-  print_log "preparing TPC-H database"
-
-  print_log "  creating tables"
-  psql -h $HOST -p $PORT -d $DBNAME -f ${ROOT}/create_table.sql > $RESULTS/create_table.log 2> $RESULTS/create_table.err
-
-  print_log "  loading data"
-
-  psql -h $HOST -p $PORT  -d $DBNAME -f ${ROOT}/load_data.sql > $RESULTS/load_data.log 2> $RESULTS/load_data.err
-
-  print_log "  creating indexes"
-
-  psql -h $HOST -p $PORT  -d $DBNAME -f ${ROOT}/create_index.sql > $RESULTS/create_index.log 2> $RESULTS/create_index.err
-
-  print_log "running TPC-H benchmark"
-
-  benchmark_dss
-
-  print_log "finished TPC-H benchmark"
-}
-
-function benchmark_dss() {
+benchmark_dss() {
   mkdir $RESULTS/explain $RESULTS/results $RESULTS/errors
-
+  echo "Called?"
   # get bgwriter stats
-  psql  -p $PORT  -d $DBNAME -c "SELECT * FROM pg_stat_bgwriter" > $RESULTS/stats-before.log 2>> $RESULTS/stats-before.err
-  psql  -p $PORT  -d $DBNAME -c "SELECT * FROM pg_stat_database WHERE datname = '$DBNAME'" >> $RESULTS/stats-before.log 2>> $RESULTS/stats-before.err
+  psql $DBNAME -c "SELECT * FROM pg_stat_bgwriter" > $RESULTS/stats-before.log 2>> $RESULTS/stats-before.err
+  psql $DBNAME -c "SELECT * FROM pg_stat_database WHERE datname = '$DBNAME'" >> $RESULTS/stats-before.log 2>> $RESULTS/stats-before.err
 
   print_log "running queries defined in TPC-H benchmark"
 
@@ -56,24 +30,55 @@ function benchmark_dss() {
 
     if [ -f "$q" ]; then
       print_log "  running query $n"
+      echo "RUNNING QUERY $n"
       # run explain
-      psql -h $HOST -p $PORT  -d $DBNAME -f $qe > $RESULTS/explain/$n 2>> $RESULTS/explain.err
-      psql -h $HOST -p $PORT  -d $DBNAME -f $q > $RESULTS/results/$n 2> $RESULTS/errors/$n
+      psql $DBNAME -f $qe > $RESULTS/explain/$n 2>> $RESULTS/explain.err
+      psql $DBNAME -f $q > $RESULTS/results/$n 2> $RESULTS/errors/$n
     fi;
+
   done;
 
   # collect stats again
-  psql -h $HOST -p $PORT -d $DBNAME -c "SELECT * FROM pg_stat_bgwriter" > $RESULTS/stats-after.log 2>> $RESULTS/stats-after.err
-  psql -h $HOST -p $PORT -d $DBNAME -c "SELECT * FROM pg_stat_database WHERE datname = '$DBNAME'" >> $RESULTS/stats-after.log 2>> $RESULTS/stats-after.err
+  psql $DBNAME -c "SELECT * FROM pg_stat_bgwriter" > $RESULTS/stats-after.log 2>> $RESULTS/stats-after.err
+  psql $DBNAME -c "SELECT * FROM pg_stat_database WHERE datname = '$DBNAME'" >> $RESULTS/stats-after.log 2>> $RESULTS/stats-after.err
 }
 
-function print_log() {
-  local message=$1
+print_log() {
+  message=$1
   echo `date +"%Y-%m-%d %H:%M:%S"` "["`date +%s`"] : $message" >> $RESULTS/$LOGFILE;
 }
 
 rm -rf $RESULTS
 mkdir -p $RESULTS
 
-# run the benchmark
-benchmark_run
+if [ "$2" == "cold" ]; then
+  # DROP and CREATE Database
+  dropdb $DBNAME
+  createdb $DBNAME
+
+  psql $DBNAME -c "select name,setting from pg_settings" > $RESULTS/settings.log 2> $RESULTS/settings.err
+
+  print_log "preparing TPC-H database"
+  print_log "  creating tables"
+  psql $DBNAME -f ${ROOT}/create_table.sql > $RESULTS/create_table.log 2> $RESULTS/create_table.err
+  echo "CREATED TABLE"
+  print_log "  loading data"
+  psql $DBNAME -f ${ROOT}/load_data.sql > $RESULTS/load_data.log 2> $RESULTS/load_data.err
+  echo "LOADED DATA"
+  print_log "  creating primary keys"
+  psql $DBNAME -f ${ROOT}/primary_key.sql> $RESULTS/primary_key.log 2> $RESULTS/primary_key.err
+  echo "CREATED PRIMARY KEY"
+  print_log "  creating foreign keys"
+  psql $DBNAME -f ${ROOT}/foreign_key.sql > $RESULTS/foreign_key.log 2> $RESULTS/foreign_key.err
+  echo "CREATED FOREIGN KEY"
+  print_log "  creating indexes"
+  psql $DBNAME -f ${ROOT}/create_index.sql > $RESULTS/create_index.log 2> $RESULTS/create_index.err
+  echo "CREATED INDEXES"
+  print_log "  analyzing"
+  psql $DBNAME -c "analyze" > $RESULTS/analyze.log 2> $RESULTS/analyze.err
+fi
+
+print_log "running TPC-H benchmark"
+benchmark_dss
+
+print_log "finished TPC-H benchmark"
